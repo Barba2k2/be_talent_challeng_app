aae3976dc4128330afa614074f67b4abf2d91f13
Lorenzo DZ <lorenzo.dz@hotmail.com>
2024-11-20 16:14:29 -0300

    feat(design_system): Add ColorsDS class with color constants

---
+++ design_system/lib/src/theme/colors_ds.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ColorsDS {
+  // Brand
+  static const primary = Color(0xFF0000FF);
+
+  // Neutral
+  static const black = Color(0xFF1C1C1C);
+  static const white = Color(0xFFFFFFFF);
+  static const gray20 = Color(0xFFE2E2E2);
+  static const gray10 = Color(0xFFF5F5F5);
+
+  // Não pode ser instanciado
+  ColorsDS._();
+}
