7c2d9d7aeeb2cdf3bceb7bb9edf069d80adf5d7e
Lorenzo DZ <lorenzo.dz@hotmail.com>
2024-11-20 17:06:41 -0300

    feat(models): Add Employee class

---
+++ lib/src/models/employee.dart
@@ -0,0 +1,13 @@
+class Employee {
+  final String name;
+  final String position;
+  final String admissionDate;
+  final String phone;
+
+  const Employee({
+    required this.name,
+    required this.position,
+    required this.admissionDate,
+    required this.phone,
+  });
+}
