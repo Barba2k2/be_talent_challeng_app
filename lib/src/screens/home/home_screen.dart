89e1b8bbdb2c1d093e1c2d7b232893ecb8c40793
Lorenzo DZ <lorenzo.dz@hotmail.com>
2024-11-20 18:39:51 -0300

    refactor(home-screen): Update home screen layout and add notification badge

--- lib/src/screens/home/home_screen.dart
+++ lib/src/screens/home/home_screen.dart
@@ -2,6 +2,9 @@ import 'package:design_system/design_system.dart';
 import 'package:flutter/material.dart';
 
 import '../../widgets/employees_list.dart';
+import '../../widgets/notifications_app_bar.dart';
+
+part 'components/employees_screen_body.dart';
 
 class EmployeesScreen extends StatelessWidget {
   const EmployeesScreen({super.key});
@@ -23,71 +26,11 @@ class EmployeesScreen extends StatelessWidget {
           actions: [
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: SpacingDS.lg),
-              child: Stack(
-                alignment: Alignment.center,
-                children: [
-                  Icon(
-                    Icons.notifications_none_rounded,
-                    color: ColorsDS.black,
-                    size: 36,
-                  ),
-                  Positioned(
-                    right: 0,
-                    top: 14,
-                    child: Container(
-                      width: 20,
-                      height: 20,
-                      decoration: const BoxDecoration(
-                        color: ColorsDS.primary,
-                        shape: BoxShape.circle,
-                      ),
-                      child: Center(
-                        child: Text(
-                          '02',
-                          textAlign: TextAlign.center,
-                          style: TypographyDS.h3.copyWith(
-                            color: ColorsDS.white,
-                            fontSize: 8,
-                            fontWeight: FontWeight.w700,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+              child: NotificationBadge(count: '02'),
             ),
           ],
         ),
-        body: Padding(
-          padding: const EdgeInsets.symmetric(
-            horizontal: SpacingDS.md,
-            vertical: SpacingDS.xl,
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Funcionários',
-                style: TypographyDS.h1.copyWith(
-                  color: ColorsDS.black,
-                ),
-              ),
-              const SizedBox(
-                height: SpacingDS.md,
-              ),
-              const SearchInputDS(
-                hint: 'Pesquisar',
-              ),
-              const SizedBox(
-                height: SpacingDS.md,
-              ),
-              Expanded(
-                child: EmployeesList(),
-              ),
-            ],
-          ),
-        ),
+        body: _EmployessScreenBody(),
       ),
     );
   }
