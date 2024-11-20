9216b3f04c5021775101d3ebb1852bbe3d14de77
Lorenzo DZ <lorenzo.dz@hotmail.com>
2024-11-20 18:25:06 -0300

    refactor(employee_model): Add optional imageUrl field to Employee class

--- lib/src/models/employee.dart
+++ lib/src/models/employee.dart
@@ -3,11 +3,13 @@ class Employee {
   final String position;
   final String admissionDate;
   final String phone;
+  final String? imageUrl;
 
   const Employee({
     required this.name,
     required this.position,
     required this.admissionDate,
     required this.phone,
+    this.imageUrl,
   });
 }
