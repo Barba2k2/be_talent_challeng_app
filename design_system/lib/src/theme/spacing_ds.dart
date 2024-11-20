6f2c6af7623b863e0a55979932730b2d7bb63e63
Lorenzo DZ <lorenzo.dz@hotmail.com>
2024-11-20 18:06:31 -0300

    refactor(theme): Update spacing values in SpacingDS

--- design_system/lib/src/theme/spacing_ds.dart
+++ design_system/lib/src/theme/spacing_ds.dart
@@ -1,9 +1,11 @@
 class SpacingDS {
   static const double xs = 4.0;
   static const double sm = 8.0;
-  static const double md = 16.0;
-  static const double lg = 24.0;
-  static const double xl = 32.0;
+  static const double md = 18.0;
+  static const double lg = 20.0;
+  static const double xl = 24.0;
+  static const double xxl = 32.0;
+  
 
   SpacingDS._();
 }
