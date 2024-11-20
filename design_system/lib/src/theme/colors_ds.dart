b8e3cf6828165c2315cc53be65e328fa058a2c35
Lorenzo DZ <lorenzo.dz@hotmail.com>
2024-11-20 16:31:42 -0300

    feat(design_system): Update primary color in colors_ds.dart

--- design_system/lib/src/theme/colors_ds.dart
+++ design_system/lib/src/theme/colors_ds.dart
@@ -2,7 +2,21 @@ import 'package:flutter/material.dart';
 
 class ColorsDS {
   // Brand
-  static const primary = Color(0xFF0000FF);
+  static const MaterialColor primary = MaterialColor(
+    0xFF0000FF, // Valor hexadecimal da cor primária
+    <int, Color>{
+      50: Color(0xFFE3E3FF),
+      100: Color(0xFFB9B9FF),
+      200: Color(0xFF8C8CFF),
+      300: Color(0xFF5F5FFF),
+      400: Color(0xFF3D3DFF),
+      500: Color(0xFF0000FF), // Cor primária
+      600: Color(0xFF0000FF),
+      700: Color(0xFF0000CC),
+      800: Color(0xFF000099),
+      900: Color(0xFF000066),
+    },
+  );
 
   // Neutral
   static const black = Color(0xFF1C1C1C);
