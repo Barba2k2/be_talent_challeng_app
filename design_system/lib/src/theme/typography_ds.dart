d69073ca0affd565332466fef042c2b72c35fa14
Lorenzo DZ <lorenzo.dz@hotmail.com>
2024-11-20 16:14:42 -0300

    feat(design_system): Add TypographyDS class with typography constants

---
+++ design_system/lib/src/theme/typography_ds.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class TypographyDS {
+  static const String _fontFamily = 'Helvetica';
+
+  static const TextStyle h1 = TextStyle(
+    fontFamily: _fontFamily,
+    fontSize: 20,
+    fontWeight: FontWeight.w500,
+    height: 1.5,
+  );
+
+  static const TextStyle h2 = TextStyle(
+    fontFamily: _fontFamily,
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+    height: 1.5,
+  );
+
+  static const TextStyle h3 = TextStyle(
+    fontFamily: _fontFamily,
+    fontSize: 15,
+    fontWeight: FontWeight.w400,
+    height: 1.5,
+  );
+
+  TypographyDS._();
+}
