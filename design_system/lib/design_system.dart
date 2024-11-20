3906352b9fac7a265a933e68c886f8014ed99e50
Lorenzo DZ <lorenzo.dz@hotmail.com>
2024-11-20 18:06:49 -0300

    refactor(design-system): Update design_system.dart exports

--- design_system/lib/design_system.dart
+++ design_system/lib/design_system.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: unnecessary_library_name
 library design_system;
 
 export 'src/theme/colors_ds.dart';
@@ -6,3 +7,4 @@ export 'src/theme/typography_ds.dart';
 export 'src/components/buttom_ts.dart';
 export 'src/components/employer_card_ts.dart';
 export 'src/components/search_imputs_ds.dart';
+export 'src/components/app_bar_ds.dart';
