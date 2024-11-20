77c4b4a521910ace778ecfe3122c1c74dd6a15ff
Lorenzo DZ <lorenzo.dz@hotmail.com>
2024-11-20 17:07:27 -0300

    feat(screens): Add SplashScreen widget and set it as the home screen

--- lib/src/app_widget.dart
+++ lib/src/app_widget.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:design_system/design_system.dart';
 
+import 'screens/splash_screen/splash_screen.dart';
+
 class AppWidget extends StatelessWidget {
   const AppWidget({super.key});
 
@@ -10,7 +12,9 @@ class AppWidget extends StatelessWidget {
       title: 'BeTalent',
       theme: ThemeData(
         primarySwatch: ColorsDS.primary,
+        fontFamily: 'Helvetica'
       ),
+      home: const SplashScreen(),
     );
   }
 }
