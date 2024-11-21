cb65ce7e399878c7196123069ee7dd184360eac6
Lorenzo DZ <lorenzo.dz@hotmail.com>
2024-11-21 00:02:11 -0300

    test(mock): Add mock EmployeesRepository for unit testing

---
+++ test/mock/mock_employees_repository.dart
@@ -0,0 +1,5 @@
+import 'package:mocktail/mocktail.dart';
+
+import 'package:be_talent_challeng_app/src/features/employees/repository/employees_repository.dart';
+
+class MockEmployeesRepository extends Mock implements EmployeesRepository {}
